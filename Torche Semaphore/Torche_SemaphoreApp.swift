109c87c8d747e8aa2c1b58d48b328fd8f75b2891
thomaslebonnec <thomaslebonnec@icloud.com>
2025-05-09 11:06:57 +0200

    Initial Commit

---
+++ Torche Semaphore/Torche_SemaphoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Torche_SemaphoreApp.swift
+//  Torche Semaphore
+//
+//  Created by Thomas Le Bonnec on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Torche_SemaphoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
